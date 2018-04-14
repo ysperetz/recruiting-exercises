e024df1d6c167586601e8f740d7cccc2520b9c1b
Yaroslav Shneykin <v-yshneykin@expedia.com>
2018-04-13 22:21:56 -0700

    Basic build setup.

---
+++ inventory-allocator/src/main/java/com/yaro/interview/allocator/Main.java
@@ -0,0 +1,12 @@
+package com.yaro.interview.allocator;
+
+/**
+ * @author v-yshneykin
+ * @since 4/13/18
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
